abb5056f982a671511de937e9df06191ed449c81
Gordon <gordonAdeacon@outlook.com>
2022-02-21 14:19:05 +0000

    seperating JS and HTML

--- Software/html/home.php
+++ Software/html/home.php
@@ -23,106 +23,7 @@ if (!isset($_SESSION['loggedin'])) {
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/paho-mqtt/1.0.1/mqttws31.min.js" type="text/javascript"></script>
 
-        <script type="text/javascript" language="javascript">
-            var mqtt;
-            var reconnectTimeout = 2000;
-            var host = "54.224.242.1"; 
-            var port = 9001;
-
-            var humidity = 0;
-            var temperature = 0;
-
-            function onConnect(){
-                //Once a connection has been made, make a subscription and send a message
-                console.log("Connected");
-                mqtt.subscribe("enviro");
-                message = new Paho.MQTT.Message("Connected!");
-                message.destinationName = "enviro";
-                mqtt.send(message);
-            }
-
-            function onFailure(){
-                console.log("Connection to " + host + ":" + port + " failed!");
-                setTimeout(mqttConnect, reconnectTimeout);
-            }
-
-            function onMessage(msg){
-                console.log(msg.payloadString);
-                var data = extractValues(msg.payloadString);
-                console.log(data);
-                if(data.length > 1){
-
-                    var tempArr = data[0];
-                    var tempTemp = tempArr[0];
-                    var temp = tempTemp[1];
-
-                    var humArr = data[2];
-                    var humHum = humArr[0];
-                    var humidity = humHum[1];
-
-                    var luxArr = data[3];
-                    var luxLux = luxArr[0];
-                    var lux = luxLux[1];
-
-                    var vpdArr = data[1];
-                    var vpdVPD = vpdArr[0];
-                    var vpd = vpdVPD[1];
-                    
-
-                    document.getElementById("tempSection").innerHTML = temp + "C"; 
-                    document.getElementById("tempSection").style.color = 'green';
-                            
-                    document.getElementById("humSection").innerHTML = humidity + "%";
-                    document.getElementById("humSection").style.color = 'green';
-                            
-                    document.getElementById("luxSection").innerHTML = lux + "lx";
-                    document.getElementById("luxSection").style.color = 'green';
-                            
-                    document.getElementById("vpdSection").innerHTML = vpd + "KPa";
-                    document.getElementById("vpdSection").style.color = 'green';
-                            
-                    setTimeout(() => { 
-                        document.getElementById("tempSection").style.color = 'black';
-                        document.getElementById("humSection").style.color = 'black';
-                        document.getElementById("luxSection").style.color = 'black';
-                        document.getElementById("vpdSection").style.color = 'black';  
-                    }, 500);
-                            
-                    
-                }
-                
-
-            }
-
-            function mqttConnect(){
-                console.log("Connecting to " + host + ":" + port);
-                mqtt = new Paho.MQTT.Client(host, port, "clientJS");
-                //document.write("connection to " + host + ":" + port);
-                var options = {
-                    timeout: 3,
-                    onSuccess: onConnect,
-                    onFailure: onFailure,
-                };
-
-                mqtt.onMessageArrived = onMessage
-
-                mqtt.connect(options); //connect
-            }
-
-            function extractValues(data){
-                console.log("Extract: " + data);
-                const KV_SEP = ": ";
-                const ENTITY_SEP = ", ";
-                var obj = {};
-
-                obj = data.split(ENTITY_SEP).map(function(val){
-                    return [val.split(KV_SEP)];
-                });
-
-                return obj;
-            }
-
-        </script> 
+        <script src="mqtt.js"></script>
     </head>
 
     <body>
@@ -180,7 +81,7 @@ if (!isset($_SESSION['loggedin'])) {
                 <div class="col border py-4" id="avgVPDSection"></div>
             </div>
         </div>
-            <script>
+            <script type="text/javascript">
                 mqttConnect();
             </script>
     </body>
